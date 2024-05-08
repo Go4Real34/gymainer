98a6966fd4b62424107b57886aac3ca3d5729291
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-05-08 03:24:22 +0300

    restructure ModelSettings class and update ConvLTSMModel class

--- project/GymAIner/structures/ConvLTSMModel.py
+++ project/GymAIner/structures/ConvLTSMModel.py
@@ -11,20 +11,25 @@ class ConvLTSMModel:
     def __init__(self, settings):
         self.settings = settings
         
-        self.all_settings = ModelSettings(dataset_path=self.settings["model"]["dataset_path"], 
-                                          sequence_length=self.settings["model"]["sequence_length"], 
-                                          validation_ratio=self.settings["model"]["validation_ratio"], 
-                                          test_ratio=self.settings["model"]["test_ratio"], 
-                                          filter_counts=self.settings["model"]["filter_counts"], 
-                                          kernel_sizes=self.settings["model"]["kernel_sizes"], 
-                                          activations=self.settings["model"]["activations"], 
-                                          data_formats=self.settings["model"]["data_formats"], 
-                                          recurrent_dropouts=self.settings["model"]["recurrent_dropouts"], 
-                                          return_sequences=self.settings["model"]["return_sequences"], 
-                                          pool_sizes=self.settings["model"]["pool_sizes"], 
-                                          paddings=self.settings["model"]["paddings"], 
-                                          dropouts=self.settings["model"]["dropouts"], 
-                                          label_count=self.settings["model"]["label_count"], 
+        return
+    
+
+    def init(self):
+        self.all_settings = ModelSettings(dataset_path=self.settings["dataset"]["path"], 
+                                          dataset_sequence_length=self.settings["dataset"]["sequence_length"], 
+                                          dataset_label_count=self.settings["dataset"]["label_count"], 
+                                          dataset_validation_ratio=self.settings["dataset"]["validation_ratio"], 
+                                          dataset_test_ratio=self.settings["dataset"]["test_ratio"], 
+                                          
+                                          model_filter_counts=self.settings["model"]["filter_counts"], 
+                                          model_kernel_sizes=self.settings["model"]["kernel_sizes"], 
+                                          model_activations=self.settings["model"]["activations"], 
+                                          model_data_formats=self.settings["model"]["data_formats"], 
+                                          model_recurrent_dropouts=self.settings["model"]["recurrent_dropouts"], 
+                                          model_return_sequences=self.settings["model"]["return_sequences"], 
+                                          model_pool_sizes=self.settings["model"]["pool_sizes"], 
+                                          model_paddings=self.settings["model"]["paddings"], 
+                                          model_dropouts=self.settings["model"]["dropouts"], 
                                           
                                           early_stopping_callback_count=self.settings["early_stopping"]["count"], 
                                           early_stopping_callback_monitors=self.settings["early_stopping"]["monitors"], 
@@ -33,75 +38,76 @@ class ConvLTSMModel:
                                           early_stopping_callback_restore_best_weights=self.settings["early_stopping"]["restore_best_weights"])
         
         self.dataset_handler = DatasetHandler(self.all_settings.get_dataset_path(), 
-                                              self.all_settings.get_sequence_length(), 
-                                              self.all_settings.get_test_ratio())
+                                              self.all_settings.get_dataset_sequence_length(), 
+                                              self.all_settings.get_dataset_test_ratio())
         
         self.model = self.create_model()
         self.early_stopping_callbacks = self.create_early_stopping_callbacks()
         
         return
     
+
     def create_model(self):
         model = Sequential()
         
-        model.add(ConvLSTM2D(filters=self.all_settings.get_filter_count(0), 
-                             kernel_size=self.all_settings.get_kernel_size(0), 
-                             activation=self.all_settings.get_activation(0), 
-                             data_format=self.all_settings.get_data_format(0), 
-                             recurrent_dropout=self.all_settings.get_recurrent_dropout(0), 
-                             return_sequences=self.all_settings.get_return_sequence(0), 
+        model.add(ConvLSTM2D(filters=self.all_settings.get_model_filter_count(0), 
+                             kernel_size=self.all_settings.get_model_kernel_size(0), 
+                             activation=self.all_settings.get_model_activation(0), 
+                             data_format=self.all_settings.get_model_data_format(0), 
+                             recurrent_dropout=self.all_settings.get_model_recurrent_dropout(0), 
+                             return_sequences=self.all_settings.get_model_return_sequence(0), 
                              input_shape = (self.dataset_handler.SEQUENCE_LENGTH, 
                                             self.dataset_handler.RESIZE_WIDTH, 
                                             self.dataset_handler.RESIZE_HEIGHT, 
                                             self.dataset_handler.COLOR_CHANNELS)))
         
-        model.add(MaxPooling3D(pool_size=self.all_settings.get_pool_size(0), 
+        model.add(MaxPooling3D(pool_size=self.all_settings.get_model_pool_size(0), 
                                padding=self.all_settings.get_padding(0), 
-                               data_format=self.all_settings.get_data_format(1)))
+                               data_format=self.all_settings.get_model_data_format(1)))
         
-        model.add(TimeDistributed(Dropout(self.all_settings.get_dropout(0))))
+        model.add(TimeDistributed(Dropout(self.all_settings.get_model_dropout(0))))
         
-        model.add(ConvLSTM2D(filters=self.all_settings.get_filter_count(1), 
-                             kernel_size=self.all_settings.get_kernel_size(1), 
-                             activation=self.all_settings.get_activation(1), 
-                             data_format=self.all_settings.get_data_format(2), 
-                             recurrent_dropout=self.all_settings.get_recurrent_dropout(1), 
+        model.add(ConvLSTM2D(filters=self.all_settings.get_model_filter_count(1), 
+                             kernel_size=self.all_settings.get_model_kernel_size(1), 
+                             activation=self.all_settings.get_model_activation(1), 
+                             data_format=self.all_settings.get_model_data_format(2), 
+                             recurrent_dropout=self.all_settings.get_model_recurrent_dropout(1), 
                              return_sequences=self.all_settings.get_return_sequences(1)))
 
-        model.add(MaxPooling3D(pool_size=self.all_settings.get_pool_size(1), 
+        model.add(MaxPooling3D(pool_size=self.all_settings.get_model_pool_size(1), 
                                padding=self.all_settings.get_padding(1), 
-                               data_format=self.all_settings.get_data_format(3)))
+                               data_format=self.all_settings.get_model_data_format(3)))
         
-        model.add(TimeDistributed(Dropout(self.all_settings.get_dropout(1))))
+        model.add(TimeDistributed(Dropout(self.all_settings.get_model_dropout(1))))
         
-        model.add(ConvLSTM2D(filters=self.all_settings.get_filter_count(2), 
-                             kernel_size=self.all_settings.get_kernel_size(2), 
-                             activation=self.all_settings.get_activation(2), 
-                             data_format=self.all_settings.get_data_format(4), 
-                             recurrent_dropout=self.all_settings.get_recurrent_dropout(2), 
+        model.add(ConvLSTM2D(filters=self.all_settings.get_model_filter_count(2), 
+                             kernel_size=self.all_settings.get_model_kernel_size(2), 
+                             activation=self.all_settings.get_model_activation(2), 
+                             data_format=self.all_settings.get_model_data_format(4), 
+                             recurrent_dropout=self.all_settings.get_model_recurrent_dropout(2), 
                              return_sequences=self.all_settings.get_return_sequences(2)))
         
-        model.add(MaxPooling3D(pool_size=self.all_settings.get_pool_size(2), 
+        model.add(MaxPooling3D(pool_size=self.all_settings.get_model_pool_size(2), 
                                padding=self.all_settings.get_padding(2), 
-                               data_format=self.all_settings.get_data_format(5)))
+                               data_format=self.all_settings.get_model_data_format(5)))
         
-        model.add(TimeDistributed(Dropout(self.all_settings.get_dropout(2))))
+        model.add(TimeDistributed(Dropout(self.all_settings.get_model_dropout(2))))
         
-        model.add(ConvLSTM2D(filters=self.all_settings.get_filter_count(3), 
-                             kernel_size=self.all_settings.get_kernel_size(3), 
-                             activation=self.all_settings.get_activation(3), 
-                             data_format=self.all_settings.get_data_format(6), 
-                             recurrent_dropout=self.all_settings.get_recurrent_dropout(3), 
+        model.add(ConvLSTM2D(filters=self.all_settings.get_model_filter_count(3), 
+                             kernel_size=self.all_settings.get_model_kernel_size(3), 
+                             activation=self.all_settings.get_model_activation(3), 
+                             data_format=self.all_settings.get_model_data_format(6), 
+                             recurrent_dropout=self.all_settings.get_model_recurrent_dropout(3), 
                              return_sequences=self.all_settings.get_return_sequences(3)))
         
-        model.add(MaxPooling3D(pool_size=self.all_settings.get_pool_size(3), 
+        model.add(MaxPooling3D(pool_size=self.all_settings.get_model_pool_size(3), 
                                padding=self.all_settings.get_padding(3), 
-                               data_format=self.all_settings.get_data_format(7)))
+                               data_format=self.all_settings.get_model_data_format(7)))
         
         model.add(Flatten())
         
-        model.add(Dense(units=self.all_settings.get_label_count(), 
-                        activation=self.all_settings.get_activation(4)))
+        model.add(Dense(units=self.all_settings.get_dataset_label_count(), 
+                        activation=self.all_settings.get_model_activation(4)))
 
         model.summary()
         plot_model(model, show_shapes=True, show_layer_names=True)
--- project/GymAIner/structures/ModelSettings.py
+++ project/GymAIner/structures/ModelSettings.py
@@ -1,19 +1,20 @@
 class ModelSettings:
     def __init__(self, 
                  dataset_path, 
-                 sequence_length, 
-                 validation_ratio, 
-                 test_ratio, 
-                 filter_counts, 
-                 kernel_sizes, 
-                 activations, 
-                 data_formats, 
-                 recurrent_dropouts, 
-                 return_sequences, 
-                 pool_sizes, 
-                 paddings, 
-                 dropouts, 
-                 label_count, 
+                 dataset_sequence_length, 
+                 dataset_label_count, 
+                 dataset_validation_ratio, 
+                 dataset_test_ratio, 
+                 
+                 model_filter_counts, 
+                 model_kernel_sizes, 
+                 model_activations, 
+                 model_data_formats, 
+                 model_recurrent_dropouts, 
+                 model_return_sequences, 
+                 model_pool_sizes, 
+                 model_paddings, 
+                 model_dropouts, 
                  
                  early_stopping_callback_count, 
                  early_stopping_callback_monitors, 
@@ -22,19 +23,20 @@ class ModelSettings:
                  early_stopping_callback_restore_best_weights):
         
         self.dataset_path = dataset_path
-        self.sequence_length = sequence_length
-        self.validation_ratio = validation_ratio
-        self.test_ratio = test_ratio
-        self.filter_counts = filter_counts
-        self.kernel_sizes = kernel_sizes
-        self.activations = activations
-        self.data_formats = data_formats
-        self.recurrent_dropouts = recurrent_dropouts
-        self.return_sequences = return_sequences
-        self.pool_sizes = pool_sizes
-        self.paddings = paddings
-        self.dropouts = dropouts
-        self.label_count = label_count
+        self.dataset_sequence_length = dataset_sequence_length
+        self.dataset_label_count = dataset_label_count
+        self.dataset_validation_ratio = dataset_validation_ratio
+        self.dataset_test_ratio = dataset_test_ratio
+        
+        self.model_filter_counts = model_filter_counts
+        self.model_kernel_sizes = model_kernel_sizes
+        self.model_activations = model_activations
+        self.model_data_formats = model_data_formats
+        self.model_recurrent_dropouts = model_recurrent_dropouts
+        self.model_return_sequences = model_return_sequences
+        self.model_pool_sizes = model_pool_sizes
+        self.model_paddings = model_paddings
+        self.model_dropouts = model_dropouts
         
         self.early_stopping_callback_count = early_stopping_callback_count
         self.early_stopping_callback_monitors = early_stopping_callback_monitors
@@ -47,45 +49,47 @@ class ModelSettings:
     def get_dataset_path(self):
         return self.dataset_path
     
-    def get_sequence_length(self):
-        return self.sequence_length
+    def get_dataset_sequence_length(self):
+        return self.dataset_sequence_length
     
-    def get_validation_ratio(self):
-        return self.validation_ratio
+    def get_dataset_label_count(self, layer):
+        return self.dataset_label_count
     
-    def get_test_ratio(self):
-        return self.test_ratio
+    def get_dataset_validation_ratio(self):
+        return self.dataset_validation_ratio
     
-    def get_filter_count(self, layer):
-        return self.filter_counts[layer]
+    def get_dataset_test_ratio(self):
+        return self.dataset_test_ratio
+    
 
-    def get_kernel_size(self, layer):
-        return self.kernel_sizes[layer]
-    
-    def get_activation(self, layer):
-        return self.activations[layer]
-    
-    def get_data_format(self, layer):
-        return self.data_formats[layer]
+    def get_model_filter_count(self, layer):
+        return self.model_filter_counts[layer]
 
-    def get_recurrent_dropout(self, layer):
-        return self.recurrent_dropouts[layer]
+    def get_model_kernel_size(self, layer):
+        return self.model_kernel_sizes[layer]
     
-    def get_return_sequence(self, layer):
-        return self.return_sequences[layer]
+    def get_model_activation(self, layer):
+        return self.model_activations[layer]
     
-    def get_pool_size(self, layer):
-        return self.pool_sizes[layer]
+    def get_model_data_format(self, layer):
+        return self.model_data_formats[layer]
+
+    def get_model_recurrent_dropout(self, layer):
+        return self.model_recurrent_dropouts[layer]
     
-    def get_paddings(self, layer):
-        return self.paddings[layer]
+    def get_model_return_sequence(self, layer):
+        return self.model_return_sequences[layer]
     
-    def get_dropout(self, layer):
-        return self.dropouts[layer]
+    def get_model_pool_size(self, layer):
+        return self.model_pool_sizes[layer]
     
-    def get_label_count(self, layer):
-        return self.label_count
+    def get_model_paddings(self, layer):
+        return self.model_paddings[layer]
     
+    def get_model_dropout(self, layer):
+        return self.model_dropouts[layer]
+    
+
     def get_early_stopping_callback_count(self):
         return self.early_stopping_callback_count
     
