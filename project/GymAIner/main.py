6aa89bd6784e3d45b5850024427288cd19d21d24
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-05-15 20:11:05 +0300

    delete trained model types and files due to underperforming

--- project/GymAIner/main.py
+++ project/GymAIner/main.py
@@ -1,120 +1,12 @@
 import os
 
-from structures import ConvLTSMModel, LRCNModel
+from structures import DatasetHandler
 
 DATASET_PATH = os.path.join("dataset", "Fit3D Video Dataset")
 
-def conv_lstm_model_execute():
-    conv_ltsm_model_settings = {
-        "dataset": {
-            "path": DATASET_PATH, 
-            "sequence_length": 150, 
-            "resize_width": 64, 
-            "resize_height": 64, 
-            "color_channels": 3, 
-            "validation_ratio": 0.2, 
-            "test_ratio": 0.2,
-        }, 
-        
-        "model": {
-            "filter_counts": [4, 8, 14, 16],
-            "kernel_sizes": [(3, 3), (3, 3), (3, 3), (3, 3)],
-            "activations": ["tanh", "tanh", "tanh", "tanh", "softmax"],
-            "data_formats": ["channels_last", "channels_last", "channels_last", "channels_last", "channels_last", "channels_last", "channels_last", "channels_last"],
-            "recurrent_dropouts": [0.2, 0.2, 0.2, 0.2],
-            "return_sequences": [True, True, True, True],
-            "pool_sizes": [(1, 2, 2), (1, 2, 2), (1, 2, 2), (1, 2, 2)],
-            "paddings": ["same", "same", "same", "same"],
-            "dropouts": [0.2, 0.2, 0.2],
-        }, 
-        
-        "early_stopping": {
-            "count": 1, 
-            "monitors": ["val_loss"], 
-            "patiences": [10], 
-            "modes": ["min"], 
-            "restore_best_weights": [True]
-        }, 
-        
-        "compiling": {
-            "loss": "categorical_crossentropy", 
-            "learning_rate": 0.001, 
-            "metrics": ["accuracy"]
-        }, 
-        
-        "training": {
-            "epochs": 50, 
-            "batch_size": 16, 
-            "shuffle": True
-        }, 
-        
-        "statistics": {
-            "graph_size": (12, 6)
-        }
-    }
-    
-    conv_ltsm_model = ConvLTSMModel(conv_ltsm_model_settings)
-    conv_ltsm_model.execute()
-    
-    return
-
-
-def lrcn_model_execute():
-    lrcn_model_settings = {
-        "dataset": {
-            "path": DATASET_PATH, 
-            "sequence_length": 150, 
-            "resize_width": 64, 
-            "resize_height": 64, 
-            "color_channels": 3, 
-            "validation_ratio": 0.2, 
-            "test_ratio": 0.2,
-        }, 
-        
-        "model": {
-            "filter_counts": [16, 32, 64, 64], 
-            "kernel_sizes": [(3, 3), (3, 3), (3, 3), (3, 3)], 
-            "paddings": ["same", "same", "same", "same"], 
-            "activations": ["relu", "relu", "relu", "relu", "softmax"], 
-            "pool_sizes": [(4, 4), (4, 4), (2, 2), (2, 2)], 
-            "dropouts": [0.25, 0.25, 0.25],
-            "lstm_unit_count": 128,
-        }, 
-        
-        "early_stopping": {
-            "count": 1, 
-            "monitors": ["val_loss"], 
-            "patiences": [15], 
-            "modes": ["min"], 
-            "restore_best_weights": [True]
-        }, 
-        
-        "compiling": {
-            "loss": "categorical_crossentropy", 
-            "learning_rate": 0.001, 
-            "metrics": ["accuracy"]
-        }, 
-        
-        "training": {
-            "epochs": 80, 
-            "batch_size": 16, 
-            "shuffle": True
-        }, 
-        
-        "statistics": {
-            "graph_size": (12, 6)
-        }
-    }
-    
-    lrcn_model = LRCNModel(lrcn_model_settings)
-    lrcn_model.execute()
-    
-    return
-
-
 def main():
-    conv_lstm_model_execute()
-    lrcn_model_execute()
+    dataset_handler = DatasetHandler(DATASET_PATH, 75, 128, 128, 3, 0.2, 0.2)
+    dataset_handler.init()
     
     return 0
 
--- project/GymAIner/structures/__init__.py
+++ project/GymAIner/structures/__init__.py
@@ -1,10 +1,3 @@
-from structures.LRCNModelSettings import LRCNModelSettings
 from .DatasetHandler import DatasetHandler
 
 from .Timer import Timer
-
-from .ConvLTSMModelSettings import ConvLTSMModelSettings
-from .ConvLTSMModel import ConvLTSMModel
-
-from .LRCNModelSettings import LRCNModelSettings
-from .LRCNModel import LRCNModel
