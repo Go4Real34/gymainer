264fcbae3bcb22b69fc1552bde97519b82ea5b5f
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-06-04 23:47:00 +0300

    initialize LSTM_ModelSettings class

---
+++ GymAIner/structures/LSTM_ModelSettings.py
@@ -0,0 +1,4 @@
+class LSTM_ModelSettings:
+    def __init__(self):
+        pass
+    
--- GymAIner/structures/__init__.py
+++ GymAIner/structures/__init__.py
@@ -3,3 +3,4 @@ from .DatasetHandler import DatasetHandler
 from .Timer import Timer
 
 from .LTSM import LSTM
+from .LSTM_ModelSettings import LTSM_ModelSettings
