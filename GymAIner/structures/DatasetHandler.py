594e8ccdb0c082f5189bf2e61c8afc43280b6ebe
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-06-05 12:16:06 +0300

    add LSTM_Model and LSTM_ModelSettings class implementations

--- GymAIner/structures/DatasetHandler.py
+++ GymAIner/structures/DatasetHandler.py
@@ -1,22 +1,35 @@
 import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = '0'
+
+import tensorflow as tf
+gpus = tf.config.list_physical_devices("GPU")
+try:
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    
+except:
+    pass
+
 import cv2
 import tqdm
-import numpy as np
 import pickle
+import numpy as np
+import matplotlib.pyplot as plt
 
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
+
 from .Timer import Timer
 
 class DatasetHandler:
-    def __init__(self, dataset_path, sequence_length, resize_width, resize_height, color_channels, validation_ratio, test_ratio):
+    def __init__(self, dataset_path, sequence_length, resize_width, resize_height, color_channel_count, validation_ratio, test_ratio):
         self.DATASET_PATH = dataset_path
         self.SEQUENCE_LENGTH = sequence_length
         self.RESIZE_WIDTH = resize_width
         self.RESIZE_HEIGHT = resize_height
-        self.COLOR_CHANNELS = color_channels
+        self.COLOR_CHANNEL_COUNT = color_channel_count
         self.VALIDATION_RATIO = validation_ratio
         self.TEST_RATIO = test_ratio
         
@@ -28,21 +41,21 @@ class DatasetHandler:
     def init(self):
         self.labeled_video_paths, self.labels, self.videos, self.included_video_paths = self.read_dataset()
         
-        self.X_train, self.X_validation, self.Y_train, self.Y_validation = train_test_split(self.videos, self.labels, test_size=self.VALIDATION_RATIO, shuffle=True)
-        self.X_validation, self.X_test, self.Y_validation, self.Y_test = train_test_split(self.X_validation, self.Y_validation, test_size=self.TEST_RATIO, shuffle=True)
+        self.videos = np.asarray(self.videos)
+        
+        label_encoder = LabelEncoder()
+        self.labels = label_encoder.fit_transform(self.labels)
+        self.labels = to_categorical(self.labels, num_classes=len(self.labeled_video_paths))
+        
+        self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(self.videos, self.labels, test_size=self.TEST_RATIO, shuffle=True)
+        self.X_train, self.X_validation, self.Y_train, self.Y_validation = train_test_split(self.X_train, self.Y_train, test_size=self.VALIDATION_RATIO, shuffle=True)
         
         self.X_train = np.asarray(self.X_train)
         self.X_validation = np.asarray(self.X_validation)
         self.X_test = np.asarray(self.X_test)
-        
-        label_encoder = LabelEncoder()
-        y_tr = label_encoder.fit_transform(self.Y_train)
-        y_va = label_encoder.fit_transform(self.Y_validation)
-        y_te = label_encoder.fit_transform(self.Y_test)
-        self.Y_train = to_categorical(y_tr)
-        self.Y_validation = to_categorical(y_va)
-        self.Y_test = to_categorical(y_te)
-
+        self.Y_train = np.asarray(self.Y_train)
+        self.Y_validation = np.asarray(self.Y_validation)
+        self.Y_test = np.asarray(self.Y_test)
 
         return
     
@@ -79,13 +92,14 @@ class DatasetHandler:
             video_list = os.listdir(folder_path)
             for video in video_list:
                 video_path = os.path.join(folder_path, video)
-                tagged_paths[category].append(video_path)
+                tagged_paths[category].append(tf.convert_to_tensor(video_path))
                 
         labels = []
         video_frames = []
         included_video_paths = []
         
-        print("Please wait while the program reads the dataset...", end="\n\n")
+        _ = self.get_mean_std_frame(np.zeros((self.SEQUENCE_LENGTH, self.RESIZE_HEIGHT, self.RESIZE_WIDTH, 1)))
+        print("\nPlease wait while the program reads the dataset...", end="\n\n")
         
         self.timer.start()
         total_videos = sum([len(v) for k, v in tagged_paths.items()])
@@ -102,10 +116,11 @@ class DatasetHandler:
                     with tqdm.tqdm(bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_noinv_fmt}]", 
                                    desc="Reading videos...", unit= " videos", total=video_count, leave=False) as pbar_videos:
                         for video_path in video_path_list:
-                            dataset_tag, main_folder, category, video_name = video_path.split('\\')
+                            decoded_video_path = video_path.numpy().decode("utf-8")
+                            dataset_tag, main_folder, category, video_name = decoded_video_path.split('\\')
                             pbar_videos.set_description(f"Reading video: \"{video_name}\"")
                             
-                            current_video_frames = self.read_video(video_path)
+                            current_video_frames = self.read_video(decoded_video_path)
                             if len(current_video_frames) == self.SEQUENCE_LENGTH:
                                 labels.append(category_index)
                                 video_frames.append(current_video_frames)
@@ -141,8 +156,8 @@ class DatasetHandler:
 
 
     def read_video(self, video_path):
-        frames = []
         video_reader = cv2.VideoCapture(video_path)
+        frames = []
         if not video_reader.isOpened():
             print("Error: Could not open video.")
             return
@@ -170,18 +185,18 @@ class DatasetHandler:
                 
         video_reader.release()
         
-        return frames
+        return self.get_mean_std_frame(frames)
     
-
+    def get_mean_std_frame(self, frames):
+        mean = tf.math.reduce_mean(frames)
+        std = tf.math.reduce_std(tf.cast(frames, tf.float32))
+        return tf.cast((frames - mean), tf.float32) / std
+    
     def process_frame(self, frame):
-        resized_frame = cv2.resize(frame, (self.RESIZE_WIDTH, self.RESIZE_HEIGHT))
+        resized_frame = tf.image.resize(frame, [self.RESIZE_HEIGHT, self.RESIZE_WIDTH])
+        gray_scaled_image = tf.image.rgb_to_grayscale(resized_frame)
+        gray_scaled_image = tf.cast(gray_scaled_image, tf.float32)
+        normalized_frame = gray_scaled_image / 255.0
         
-        if self.COLOR_CHANNELS == 1:
-            gray_scaled_image = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2GRAY)
-            normalized_frame = gray_scaled_image / 255.0
-            
-        else:
-            normalized_frame = resized_frame / 255.0
-            
         return normalized_frame
     
