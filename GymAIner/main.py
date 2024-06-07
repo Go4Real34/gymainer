01df23c31982c63e598e5a8e0247f10c49cced7b
Go4Real34 <gorkemsarikaya2017@gmail.com>
2024-06-07 07:50:59 +0300

    update main file to be able to play game

--- GymAIner/main.py
+++ GymAIner/main.py
@@ -11,12 +11,15 @@ def main():
     elif os.name == "nt":
         venv_python = os.path.join(cwd, "venv", "Scripts", "python")
         
-    option = input("Do you want to train or test the model (TRAIN / TEST)?: ")
+    option = input("Do you want to train or test the model (TRAIN / TEST / GAME)?: ")
     if option.upper() == "TRAIN":
         subprocess.run([venv_python, "model_train.py"])
     
     elif option.upper() == "TEST":
         subprocess.run([venv_python, "model_test.py"])
+    
+    elif option.upper() == "GAME":
+        subprocess.run([venv_python, "model_game.py"])
     
     else:
         print("Invalid option!")
